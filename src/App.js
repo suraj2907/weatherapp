1f14c7ea4b99bd04500b2147aa7a8cb67a6db974
Suraj JAwrani <surajjawrani2022@gmail.com>
2023-07-06 15:50:58 +0530

    design completed

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Weather from "./Components/Weather";
+import Weather from './Components/weather';
 
-const API_KEY = 'ce2c5a45a2bb4441912103052230507';
+// const API_KEY = 'ce2c5a45a2bb4441912103052230507';
 
 function App() {
   const [data, setData] = useState([]);
 
  
 useEffect(()=>{
+  try{
+    // var geolocation = require('geolocation')
+    // geolocation.getCurrentPosition(function (err, position) {
+    //   if (err) throw err
+    //   console.log(position)
+    // })
 const lat = () => {
+  
   navigator.geolocation.getCurrentPosition(position => position.coords.latitude);
 };
 
 const long = () => {
+ 
   navigator.geolocation.getCurrentPosition(position => position.coords.long);
 };
 
@@ -24,11 +32,15 @@ console.log( long);
 
 
       const fetchData =async () => {
-        const {data} = await axios.get(`http://api.weatherapi.com/v1/current.json?key=ce2c5a45a2bb4441912103052230507&q=${lat ,long}&aqi=no`);
+        const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=52&lon=50&appid=a9959ac92af0f0dad188e5f2f2b431a4`);
         console.log(data);
         setData(data);
       }
       fetchData();
+    }
+    catch(err) {
+           console.log("Error" , err);
+    }
   },[]);
 
 
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,16 +1,79 @@
 import React from "react";
+import "../App.css";
+import { Container, Card, CardBody } from "reactstrap";
+import moment from "moment/moment";
 
+const Weather = ({ data }) => {
 
+      const temp = ((Math.round(data.main?.temp))- 32) * 5/9;
+      const temp_max = ((Math.round(data.main?.temp_max))- 32) * 5/9;
+      const temp_min = ((Math.round(data.main?.temp_min))- 32) * 5/9;
 
-const Weather = ({data}) => {
-return (
-<div>
-       <h1> {data.location?.country}</h1>
-       <p>{data.condition?.text}</p>
-       <p className="text-black">{data.current?.temp_c}</p>
-</div>
-);
-}
+      var iconcode = data.weather[0]?.icon;
+      var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+  return (
+    <Container
+      fluid
+      style={{
+        border: "2px solid black",
+        display: "flex",
+        alignItems: "center",
+        width: "50vw",
+        height: "60vh",
+        margin: "auto",
+        marginTop: "10rem",
+        
+      }}
+      className="col box-con back"
+    >
+      <div className="flex justify col">
+        <h2>{data.name}  </h2>
+        <p style={{
+          fontSize:"30px"
+        }}>{temp} &deg;C</p>
+        <p> {data.weather[0]?.main} <span style={{
+          width:"50px",
+          height:"50px"
+        }}>{iconurl} </span> </p>
+        <div className="flex row justify">
+          <p>{temp_max} &deg;C
+          </p>
+          <p>{temp_min} &deg;C</p>
+        </div>
+      </div>
 
+<div className="flex justify box-bottom gap row"
+style={{
+borderTop:"2px solid black",
+width:"50vw",
+marginTop:"7rem",
+justifyContent:"space-evenly"
+
+}}
+
+>
+      <div className="flex col">
+        <p>Humidity:  </p>
+        <p>{data.main?.humidity} % </p>
+        </div>
+        <div className="flex  col">
+        <p>Pressure:  </p>
+        <p>{data.main?.pressure} </p>
+        </div>
+        <div className="flex col">
+        <p>Sunrise:  </p>
+        <p>{new Date(data.sys?.sunrise * 1000).toLocaleTimeString('en-IN') } </p>
+        </div>
+        <div className="flex  col">
+        <p>Sunset:   </p>
+        <p>{new Date(data.sys?.sunset * 1000).toLocaleTimeString('en-IN')} </p>
+        </div>
+        </div>
+     
+    </Container>
+  );
+};
 
 export default Weather;
+{/* <p> Sunrise:  {new Date(data.sys?.sunrise * 1000).toLocaleTimeString('en-IN')}</p> */}
+      //  <p> Sunset:  {new Date(data.sys?.sunset * 1000).toLocaleTimeString('en-IN')}</p>
