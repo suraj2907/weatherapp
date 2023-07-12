7aa86f9419e12ce9986fff5e8d4b4a1a8cd34d33
Suraj Jawrani <surajjawrani2022@gmail.com>
2023-07-13 00:53:24 +0530

    made it responsive

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -4,10 +4,6 @@ import { Container } from "reactstrap";
 import moment from "moment/moment";
 
 const Weather = ({ data }) => {
-  // const temp = (Math.round(data.main?.temp) - 32) * 5/9;
-  // const temp_max = (Math.round(data.main?.temp_max) - 32) * 5/9;
-  // const temp_min = (Math.round(data.main?.temp_min) - 32) * 5/9;
-
   const makeurl = () => {
     if (data.weather && data.weather.length > 0) {
       const iconcode = data.weather?.[0]?.icon;
@@ -17,93 +13,40 @@ const Weather = ({ data }) => {
   };
 
   return (
-    <Container
-      fluid
-      style={{
-        border: "2px solid black",
-        display: "flex",
-        alignItems: "center",
-        width: "max-content",
-        height: "max-content",
-        margin: "auto",
-        marginTop: "10rem",
-      }}
-      className="col box-con back"
-    >
-      <div className="flex justify col">
-        <div className="flex row justify">
-          <h2>{data.name} </h2>
-          <p style={{ flexDirection: "row", display: "flex" }}>
-            {moment().format("LL")}
-          </p>
+    <Container fluid className="container">
+      <div className="weather-container">
+        <div className="weather-header">
+          <h2>{data.name}</h2>
+          <p>{moment().format("LL")}</p>
         </div>
-        <p
-          style={{
-            fontSize: "30px",
-          }}
-        >
-          {data.main?.temp} &deg;C
-        </p>
-        <img
-          style={{
-            width: "80px",
-            height: "80px",
-          }}
-          src={makeurl()}
-          alt="weather-icon"
-        />{" "}
-        <h5 style={{ fontSize: "20px" }}> {data.weather?.[0]?.main} </h5>
-        <div className="flex row " style={{ gap: "30px" }}>
-          <p
-            style={{
-              fontWeight: "600",
-              justifyContent: "flex-start",
-              display: "flex",
-            }}
-          >
-            Temp-max :-{" "}
-            <span style={{ fontWeight: "500" }}>
-              {data.main?.temp_max} &deg;C{" "}
-            </span>
-          </p>
-          <p style={{ fontWeight: "600" }}>
-            Temp-min :-{" "}
-            <span style={{ fontWeight: "500" }}>
-              {" "}
-              {data.main?.temp_min} &deg;C{" "}
-            </span>
-          </p>
-        </div>
-      </div>
-
-      <div
-        className="flex justify box-bottom gap row box-bottom"
-        style={{
-          borderTop: "2px solid black",
-          width: "max-content",
-          // marginTop: "7rem",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <div className="flex col">
-          <p>Humidity: </p>
-          <p>{data.main?.humidity} % </p>
-        </div>
-        <div className="flex  col">
-          <p>Wind speed: </p>
-          <p>{data.wind?.speed} m/s</p>
-        </div>
-        <div className="flex col">
-          <p>Sunrise: </p>
+        <p className="temperature">{data.main?.temp} &deg;C</p>
+        <img className="weather-icon" src={makeurl()} alt="weather-icon" />
+        <h5 className="weather-description">{data.weather?.[0]?.main}</h5>
+        <div className="temperature-details">
           <p>
-            {new Date(data.sys?.sunrise * 1000).toLocaleTimeString("en-IN")}{" "}
+            Temp-max: <span>{data.main?.temp_max} &deg;C</span>
+          </p>
+          <p>
+            Temp-min: <span>{data.main?.temp_min} &deg;C</span>
           </p>
         </div>
-        <div className="flex  col">
-          <p>Sunset: </p>
-          <p>
-            {new Date(data.sys?.sunset * 1000).toLocaleTimeString("en-IN")}{" "}
-          </p>
+        <div className="weather-info">
+          <div className="weather-info-item">
+            <p>Humidity:</p>
+            <p>{data.main?.humidity} %</p>
+          </div>
+          <div className="weather-info-item">
+            <p>Wind speed:</p>
+            <p>{data.wind?.speed} m/s</p>
+          </div>
+          <div className="weather-info-item">
+            <p>Sunrise:</p>
+            <p>{new Date(data.sys?.sunrise * 1000).toLocaleTimeString("en-IN")}</p>
+          </div>
+          <div className="weather-info-item">
+            <p>Sunset:</p>
+            <p>{new Date(data.sys?.sunset * 1000).toLocaleTimeString("en-IN")}</p>
+          </div>
         </div>
       </div>
     </Container>
